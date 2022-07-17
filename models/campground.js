bea331a71199fd50ef6b01f3aba54597410b45e6
Ekin Kar <ekin1260@hotmail.com>
2022-07-17 19:26:14 +0300

    added image upload-delete features

--- models/campground.js
+++ models/campground.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
+const { cloudinary } = require("../cloudinary");
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_220");
+});
 
 const CampgroundSchema = new Schema({
   title: String,
-  image: String,
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
@@ -27,6 +37,9 @@ CampgroundSchema.post("findOneAndDelete", async function (doc) {
         $in: doc.reviews,
       },
     });
+    for (let img of doc.images) {
+      await cloudinary.uploader.destroy(img.filename);
+    }
   }
 });
 
--- seeds/index.js
+++ seeds/index.js
@@ -11,38 +11,32 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-async function seedImg() {
-  try {
-    const resp = await axios.get("https://api.unsplash.com/photos/random", {
-      params: {
-        client_id: "QM7RUXBTmtBNuyvsUiCXrq5SdvdVUfHtVf_3_y8yVio",
-        collections: 1114848,
-      },
-    });
-    return resp.data.urls.small;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 25; i++) {
-    // setup
+  for (let i = 0; i < 10; i++) {
     const placeSeed = Math.floor(Math.random() * places.length);
     const descriptorsSeed = Math.floor(Math.random() * descriptors.length);
     const citySeed = Math.floor(Math.random() * cities.length);
-    const priceSeed = Math.floor(Math.random() * 30);
+    const priceSeed = Math.floor(Math.random() * 30 + 10);
     const camp = new Campground({
-      image: await seedImg(),
       author: "62d3094249b13a851943e373",
       title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
       location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
       description:
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!",
       price: priceSeed,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dir50a6kj/image/upload/v1658074131/YelpCamp/image-1_qoypzz.png",
+          filename: "YelpCamp/image-1_qoypzz",
+        },
+        {
+          url: "https://res.cloudinary.com/dir50a6kj/image/upload/v1658074126/YelpCamp/image-2_qvti4k.jpg",
+          filename: "YelpCamp/image-2_qvti4k",
+        },
+      ],
     });
 
     await camp.save();
