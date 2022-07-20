321f0ee429c09e23b096e06067d1b8c0b60612eb
Ekin Kar <ekin1260@hotmail.com>
2022-07-20 12:02:11 +0300

    ready to deploy

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
+const dburl = process.env.DB_URL || "mongodb://localhost:27017/yelpCamp";
+const MongoDBStore = require("connect-mongo");
+const secret = process.env.SECRET || "isthissecret";
 
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-mongoose.connect("mongodb://localhost:27017/yelpCamp");
+mongoose.connect(dburl);
 
 const db = mongoose.connection;
 db.on("erroe", console.error.bind(console, "connection error"));
@@ -39,9 +42,18 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
+const store = MongoDBStore.create({
+  mongoUrl: dburl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
+});
+
 const sessionConfig = {
+  store,
   name: "session",
-  secret: "testSecretWord",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
