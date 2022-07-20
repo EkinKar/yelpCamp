6761bf9be3d29a0b895bd495f0dee15e1d2d7e27
Ekin Kar <ekin1260@hotmail.com>
2022-07-20 12:55:15 +0300

    last fixes before deploying

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const cities = require("./cities");
-const axios = require("axios");
 const { places, descriptors } = require("./seedhelpers");
 const Campground = require("../models/campground");
-mongoose.connect("mongodb://localhost:27017/yelpCamp");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("erroe", console.error.bind(console, "connection error"));
